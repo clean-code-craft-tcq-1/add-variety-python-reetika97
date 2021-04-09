97ea61b5e3bd1c912b524af2af7b7d826f63aa32
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-04-09 14:24:08 +0530

    Update typewise_alert.py

--- typewise_alert.py
+++ typewise_alert.py
@@ -1,5 +1,26 @@
+batteryChar={'coolingType':'PASSIVE_COOLING'}
 lowerLimit={'PASSIVE_COOLING':0,'HI_ACTIVE_COOLING':0,'MED_ACTIVE_COOLING':0, 'DEFAULT':0}
 upperLimit={'PASSIVE_COOLING':35,'HI_ACTIVE_COOLING':45,'MED_ACTIVE_COOLING':40, 'DEFAULT':0}
+
+def send_to_controller(breachType):
+  header = 0xfeed
+  print(f'{header}, {breachType}')
+
+def send_to_console(breachType):
+  print(f' The temprature is : {breachType}')
+
+def send_to_email(breachType):
+  recepient = "a.b@c.com"
+  if breachType == 'TOO_LOW':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too low')
+  elif breachType == 'TOO_HIGH':
+    print(f'To: {recepient}')
+    print('Hi, the temperature is too high')
+
+
+alertTargets={'TO_CONTROLLER':send_to_controller,'TO_EMAIL':send_to_email,'TO_CONSOLE':send_to_console}
+
 
 def infer_breach(value, lowerLimit, upperLimit):
   if value < lowerLimit:
@@ -16,22 +37,5 @@ def classify_temperature_breach(coolingType, temperatureInC):
 def check_and_alert(alertTarget, batteryChar, temperatureInC):
   breachType =\
     classify_temperature_breach(batteryChar['coolingType'], temperatureInC)
-  if alertTarget == 'TO_CONTROLLER':
-    send_to_controller(breachType)
-  elif alertTarget == 'TO_EMAIL':
-    send_to_email(breachType)
+  alertTargets[alertTarget](breachType)
 
-
-def send_to_controller(breachType):
-  header = 0xfeed
-  print(f'{header}, {breachType}')
-
-
-def send_to_email(breachType):
-  recepient = "a.b@c.com"
-  if breachType == 'TOO_LOW':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too low')
-  elif breachType == 'TOO_HIGH':
-    print(f'To: {recepient}')
-    print('Hi, the temperature is too high')
